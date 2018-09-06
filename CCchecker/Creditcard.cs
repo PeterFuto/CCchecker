43eeb1f42902472338362a2e15e0b00a37d2c915
PeterFuto <futo.petr@gmail.com>
2018-09-06 21:54:31 +0200

    Created Creditcard class

---
+++ CCchecker/Creditcard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCchecker
+{
+    class Creditcard
+    {
+        public long CCnumber { get; set; }
+        public int IndustryID { get; set; }
+        public int IssuerID { get; set; }
+        public int AccountNumber { get; set; }
+        public int Checksum { get; set; }
+
+        public Creditcard(string ccnumber)
+        {
+            CCnumber = Convert.ToInt64(ccnumber);
+            IndustryID = Convert.ToInt32(ccnumber.Substring(0,1));
+            IssuerID = Convert.ToInt32(ccnumber.Substring(1, 5));
+            AccountNumber = Convert.ToInt32(ccnumber.Substring(6, 9));
+            Checksum = Convert.ToInt32(ccnumber.Substring(15, 1));
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Card number is: {CCnumber}");
+            Console.WriteLine($"Card industry ID is: {IndustryID}");
+            Console.WriteLine($"Card issuer ID is: {IssuerID}");
+            Console.WriteLine($"Card account number is: {AccountNumber}");
+            Console.WriteLine($"Card checksum is: {Checksum}");
+        }
+    }
+
+}
---
+++ CCchecker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCchecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creditcard cc = new Creditcard("4578423013769219");
+            cc.DisplayInfo();
+            Console.ReadLine();
+        }
+    }
+}
